e11cfaccc1211a6f2d0b1ee4991b69960e642693
Makan Azarshahy <oro1919@gmail.com>
2018-07-19 19:36:54 -0700

    Added comments to app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const morgan = require('morgan');
+// --- REMOVE MORGAN PRODUCTION --- //
+const morgan = require('morgan'); // eslint-disable-line import/no-extraneous-dependencies
 
 const app = express();
 
