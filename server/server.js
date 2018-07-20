c1c7e2abfcdacef4810d01e87cd7feff36ad06ab
Makan Azarshahy <oro1919@gmail.com>
2018-07-19 19:53:50 -0700

    Added eslint ignore on console line for server listen

--- server/server.js
+++ server/server.js
@@ -3,4 +3,4 @@ const server = require('./app');
 // --- DEFAULT 1337 --- //
 const PORT = 1337;
 
-server.listen(1337, () => console.log(`SERVER is listening to PORT ${PORT}`));
+server.listen(1337, () => console.log(`SERVER is listening to PORT ${PORT}`)); // eslint-disable-line no-console
