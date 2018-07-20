732a6fc5181c037b98246b97c5e6f845914eea8e
Makan Azarshahy <oro1919@gmail.com>
2018-07-19 19:47:19 -0700

    Add config file for webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './client/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, './public/dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react', 'stage-2'],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+};
