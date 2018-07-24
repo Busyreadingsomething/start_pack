f465b547072fb73107035943a27b04a6db5a2836
Makan Azarshahy <oro1919@gmail.com>
2018-07-23 18:46:57 -0700

    Added ignore comments for linting on line 3 and 6

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'; // eslint-disable-line no-unused-vars
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app')); // eslint-disable-line no-undef
